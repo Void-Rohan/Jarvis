fea12e5dfe360d549c5e8f0fede1e86dcfbea33c
Void-Rohan <seikhrohan692@gmail.com>
2025-12-18 19:27:08 +0600

    some basic function added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
+import musicLibrary
 
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
@@ -11,22 +12,54 @@ def speak(text):
     engine.runAndWait()
 
 
+def processCommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://facebook.com")
+    elif "open insta" in c.lower():
+        webbrowser.open("https://instagram.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://instagram.com")
+    elif "open gmail" in c.lower():
+        webbrowser.open("https://gmail.com")
+    elif "open discord" in c.lower():
+        webbrowser.open("https://discord.com")
+    elif "open hianime" in c.lower():
+        webbrowser.open("https://hianime.com")
+    elif "open chatgpt" in c.lower():
+        webbrowser.open("https://chatgpt.com")
+    elif "open deepseek" in c.lower():
+        webbrowser.open("https://chat.deepseek.com")
+    elif "open chatgpt" in c.lower():
+        webbrowser.open("https://chatgpt.com")
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ")[1]
+        musicLibrary.music(song)
+
 if __name__ == "__main__":
     speak("Initializing Jarvis...")
     while True:
-        # Listen for the wakeup call "Jarvis"
+
         # obtain audio from the microphone
         r = sr.Recognizer()
-        with sr.Microphone() as source:
-            print("Listening...")
-            audio = r.listen(source)
 
-            
-        # recognize speech using Sphinx
+        # recognize speech using google
+        print("Recognizing...")
         try:
-           command = r.recognize_sphinx(audio)
-           print(command)
-        except sr.UnknownValueError:
-            print("Sphinx could not understand audio")
-        except sr.RequestError as e:
-            print("Sphinx error; {0}".format(e))
+            # Listen for the wakeup call "Jarvis"
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source, timeout=2, phrase_time_limit=1)
+            word = r.recognize_google(audio)
+            if(word.lower() == "jarvis"):
+                speak("Yes")
+                # Listen for command
+                with sr.Microphone() as source:
+                    print("Jarvis Active...")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+
+                    processCommand(c)
+        except Exception as e:
+            print("Error; {0}".format(e))
---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music = {
+    "Husn": "https://www.youtube.com/watch?v=gJLVTKhTnog&list=RDgJLVTKhTnog&start_radio=1",
+    "Jo tum mere ho": "https://www.youtube.com/watch?v=ilNt2bikxDI&list=RDilNt2bikxDI&start_radio=1",
+    "Tere pyar mein": "https://www.youtube.com/watch?v=kfKP4o-bXzM&list=RDkfKP4o-bXzM&start_radio=1"
+}
